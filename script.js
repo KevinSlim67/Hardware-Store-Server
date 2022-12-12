3967d6ab863ade44d98df1f8b67d405694313623
KevinSlim67 <kevinslim167@gmail.com>
2022-12-12 21:04:41 +0200

    fixed cors

--- script.js
+++ script.js
@@ -11,23 +11,11 @@ const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
 db.once('open', () => console.log("Connected to database"));
 
-//allows communication between server and frontend
-app.use(cors());
 
 // Add headers before the routes are defined
-app.use(function (req, res, next) {
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://mr-robot-hardware-store.netlify.app/');
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    // Pass to next layer of middleware
-    next();
-});
+app.use(
+    cors({origin: ['http://localhost:3000', 'https://mr-robot-hardware-store.netlify.app/']})
+);
 
 //allow express server to use json
 app.use(express.json());
